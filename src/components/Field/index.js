14d19bfa0fdb0615954abaa31a0addde0b14f5b7
kamil <kf404@yandex.ru>
2023-02-02 23:54:12 +0300

    up

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,40 +1,74 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 
-const initialField = Array(9).fill(null)
+const initialField = Array(9).fill(null);
+let finished = false
 
 function Field() {
     const [field, setField] = useState(initialField);
+
     const [xIsNext, setXIsNext] = useState(true);
+
+
+    useEffect(() => {
+        if (field.filter((cell) => cell === null).length <= 4) {
+            const winVariants = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+
+            for (let i = 0; i < winVariants.length; i++) {
+                if (
+                    field[winVariants[i][0]] != null &&  ( field[winVariants[i][0]] === field[winVariants[i][1]] &&
+                    field[winVariants[i][0]] === field[winVariants[i][2]] )
+                ) {
+                    finished = true
+                    console.log(`Победили ${field[winVariants[i][0]]}`)
+                    console.log(finished)
+                } else {
+                    console.log('Ничья')
+                }
+            }
+        }
+    }, [field]);
+
     const handleCellClick = (index) => {
-        const newField = [...field];
-        if (newField[index] === null) {
-            newField[index] = xIsNext ? 'X' : 'O';
+        if (field[index] === null) {
+            const newField = [...field];
+            newField[index] = xIsNext ? "X" : "O";
             setField(newField);
-            setXIsNext(!xIsNext)
+            setXIsNext(!xIsNext);
         }
     };
-
+    const restartClick = () => {
+        setField(initialField)
+        setXIsNext(true)
+    }
     return (
-        <div>
-
-        <div className="field">
-
-            {field.map((cell, index) => (
-                <button
-                    onClick={() => {
-                        handleCellClick(index);
-                    }}
-                    className="cell"
-                >
-                    {cell}
-                </button>
-            ))}
-        </div>
-            <div className="resetBlock">
-                <button className="resetButton">Restart</button>
+        <div className="container">
+            <p className="who-plays">Ходит {xIsNext ? "X" : "O"}</p>
+            <div className="field">
+                {field.map((cell, index) => (
+                    <button
+                        disabled={finished }
+                        onClick={() => {
+                            handleCellClick(index);
+                        }}
+                        className="cell"
+                    >
+                        {cell}
+                    </button>
+                ))}
             </div>
-
+            <div className="resetBlock">
+                <button onClick={() => {restartClick();}} className="restartButton">Restart</button>
+            </div>
         </div>
     );
 }
