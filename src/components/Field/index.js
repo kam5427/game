995e58ef76315fcb2c0f900722b2bd4805ee462c
kamil <kf404@yandex.ru>
2023-02-11 17:45:44 +0300

    up

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -2,13 +2,11 @@ import { useEffect, useState } from "react";
 import "./styles.css";
 
 const initialField = Array(9).fill(null);
-let finished = false
 
 function Field() {
     const [field, setField] = useState(initialField);
-
+    const [finished, setFinished] = useState(false);
     const [xIsNext, setXIsNext] = useState(true);
-
 
     useEffect(() => {
         if (field.filter((cell) => cell === null).length <= 4) {
@@ -25,49 +23,74 @@ function Field() {
 
             for (let i = 0; i < winVariants.length; i++) {
                 if (
-                    field[winVariants[i][0]] != null &&  ( field[winVariants[i][0]] === field[winVariants[i][1]] &&
-                    field[winVariants[i][0]] === field[winVariants[i][2]] )
+                    field[winVariants[i][0]] !== null &&
+                    field[winVariants[i][0]] === field[winVariants[i][1]] &&
+                    field[winVariants[i][0]] === field[winVariants[i][2]]
                 ) {
-                    finished = true
-                    console.log(`Победили ${field[winVariants[i][0]]}`)
-                    console.log(finished)
-                } else {
-                    console.log('Ничья')
+                    console.log(`Победили ${field[winVariants[i][0]]}`);
+                    setFinished(true);
+                    return;
                 }
             }
+
+            if (!field.includes(null)) {
+                console.log("Ничья");
+                setFinished(true);
+                return;
+            }
         }
+
+        // Тут ставить O с помощью бота
     }, [field]);
 
     const handleCellClick = (index) => {
         if (field[index] === null) {
             const newField = [...field];
-            newField[index] = xIsNext ? "X" : "O";
+            // newField[index] = xIsNext ? "X" : "O";
+            newField[index] = "X";
+
+            let botIndex = index;
+            while (newField[botIndex] !== null) {
+                botIndex = Math.floor(Math.random() * 8);
+            }
+            newField[botIndex] = "O";
             setField(newField);
             setXIsNext(!xIsNext);
         }
     };
+
     const restartClick = () => {
-        setField(initialField)
-        setXIsNext(true)
-    }
+        setField(initialField);
+        setXIsNext(true);
+        setFinished(false);
+    };
+
     return (
         <div className="container">
+            {/*Тут писать, кто победил */}
             <p className="who-plays">Ходит {xIsNext ? "X" : "O"}</p>
             <div className="field">
                 {field.map((cell, index) => (
                     <button
-                        disabled={finished }
                         onClick={() => {
                             handleCellClick(index);
                         }}
                         className="cell"
+                        disabled={finished}
                     >
                         {cell}
                     </button>
                 ))}
             </div>
             <div className="resetBlock">
-                <button onClick={() => {restartClick();}} className="restartButton">Restart</button>
+                <button
+                    onClick={() => {
+                        restartClick();
+                    }}
+                    className="restartButton"
+                >
+                    Restart
+                </button>
             </div>
         </div>
     );
